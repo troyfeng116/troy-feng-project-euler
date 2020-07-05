4030c97d6b80845f8f4d20e6b1d43f9ed57597d3
Troy Feng <troy.feng@yale.edu>
2020-07-05 19:45:45 -0400

    Imported template class and added to gitignore, created class for problem 51 with problem statement

---
+++ Euler051.java
@@ -0,0 +1,34 @@
+/* -------- UNSOLVED -------- */
+
+/* By replacing the 1st digit of *3, it turns out that six of the nine possible values: 13, 23, 43, 53, 73, 
+ * and 83 are all prime.
+ * 
+ * By replacing the 3rd and 4th digits of 56**3 with the same digit, this 5-digit number is the first example 
+ * having seven primes among the ten generated numbers, yielding the family: 56003, 56113, 56333, 56443, 56663, 
+ * 56773, and 56993. Consequently 56003, being the first member of this family, is the smallest prime with this 
+ * property.
+ * 
+ * Find the smallest N-digit prime which, by replacing K digits of the number (not necessarily adjacent digits) 
+ * with the same digit, is part of an L prime value family. 
+ * 
+ * Note 1: It is guaranteed that solution does exist.
+ * Note 2: Leading zeros should not be considered.
+ * Note 3: If there are several solutions, choose the "lexicographically" smallest one (one sequence is 
+ * considered "lexicographically" smaller than another if its first element which does not match the corresponding 
+ * element in another sequence is smaller) */
+
+import java.util.Scanner;
+
+public class Euler051 {
+	
+	/* Thoughts/approach: */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = Integer.parseInt(s.nextLine());
+		for (int t0 = 0; t0 < t; t0++) {
+			
+		}
+		s.close();
+	}
+}

41890e353ecbf31cc99570adde4064ca6d5c9c6a
Troy Feng <troy.feng@yale.edu>
2020-07-07 11:21:03 -0400

    Implemented prime sieve and wrote notes on approach

--- Euler051.java
+++ Euler051.java
@@ -24,9 +24,37 @@ import java.util.Scanner;
 
 public class Euler051 {
 	
-	/* Thoughts/approach: */
+	/* Thoughts/approach: first we can sieve primes up to 10^7 (to cover all seven-digit numbers). 
+	 * Let N=7, K=3, L be fixed for example. One logical way to approach the problem is to loop through all
+	 * N-K or four-digit integers (0000 -> 9999), and try all ways to place 3 stars between them. The order
+	 * (for lexicographic order) would be ***abcd -> abcd***. If a or ab or abc or abcd is zero, then that
+	 * portion can't go in the front. And if d is even, then d can't be in the back. Ex. for abcd=2153, we
+	 * would try starting from ***2153 since 1112153 would be smallest starting point in that family. Then
+	 * after trying all combinations with a * in the first place, we would move on to 2***153, etc. For each
+	 * abcd, there are maximum CHOOSE(7,3) arrangements of stars to try. We can test each combination in
+	 * 10 O(1) steps (ex. for ***2153, we would increment by 1110000 in testing for primality).
+	 *
+	 * abcd=0000 is a tricky edge case (I think a, ab, abc, abcd = 0are all tricky for preserving 
+	 * lexicographically ordered checking). Cases where a=1 are also tricky, because all stars in front don't
+	 * give the lexicographically smallest. */
+
+	final static int MAX = 10000000;
+	static boolean[] composite;
+
+	/* Sieve primes up to MAX. */
+	public static void sieve() {
+		composite = new boolean[MAX];
+		for (int i = 2; i < MAX; i++) {
+			if (!composite[i] && i <= Math.sqrt(MAX)) {
+				for (int j = i*i; j < MAX; j+=i) {
+					composite[j] = true;
+				}
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
+		sieve();
 		Scanner s = new Scanner(System.in);
 		String[] inputs = s.nextLine().split(" ");
 		int n = Integer.parseInt(inputs[0]);

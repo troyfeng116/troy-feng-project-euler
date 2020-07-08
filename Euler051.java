0a9275373a63ddaaa24493c04161993ba51580ae
Troy Feng <troy.feng@yale.edu>
2020-07-08 19:12:57 -0400

    Completed binomial combination-generating function

--- Euler051.java
+++ Euler051.java
@@ -40,7 +40,11 @@ public class Euler051 {
 	 *
 	 * I'm thinking of writing a function that takes N and K and returns an array of boolean arrays that
 	 * represent arrangements of digits and stars. The array would contain N choose K arrays of length N, each
-	 * with K trues distributed throughout representing stars. */
+	 * with K trues distributed throughout representing stars. Worst case for number of combinations would
+	 * be 7 choose 3 = 35 combinations, each of which must worst case tested with each of 0000 through 9999 for
+	 * 10000 total four-digit possibilities, each of which in turn has at most 10 primality tests, for worst 
+	 * case 3.5 million O(1) operations. Generating the base 7-digit number to begin with is worst case one loop
+	 * through a length 7 boolean array. In all, this shoudn't time out. */
 
 	final static int MAX = 10000000;
 	static int[] tenToThe;
@@ -67,12 +71,34 @@ public class Euler051 {
 		}
 	}
 
-	/* Generate all ways to arrange K trues and N-K falses. */
+	/* Generate all ways to arrange K trues and N-K falses. T corresponds to star digit. */
 	public static boolean[][] generateStarArrangements(int N, int K) {
 		boolean[][] ans = new boolean[choose(N,K)][N];
 		for (int i = 0; i < K; i++) ans[0][i] = true;
-
+		for (int i = 1; i < ans.length; i++) {
+			nextCombination(ans[i-1],ans[i]);
+		}
 		return ans;
+	}
+
+	/* Given that arr1 holds some combination of T and F, stores next reverse lexicographical permutation of
+	 * T and F (T=1, F=0) in arr2. Pre-condition: arr2 is all F. ex. arr1 = 1110000 -> arr2 = 1101000, and
+	 * arr1 = 001011 -> arr2 = 000111. */
+	public static void nextCombination(boolean[] arr1, boolean[] arr2) {
+		int n = arr1.length;
+		int k = n-2;
+		while (k >= 0 && !(arr1[k] && !arr1[k+1])) k--;
+		if (k < 0) return;
+		arr2[k] = false;
+		arr2[k+1] = true;
+		/* Copy from 1->k to arr2. */
+		for (int i = 0; i < k; i++) {
+			arr2[i] = arr1[i];
+		}
+		/* Copy reverse arr1 from k+2->n to arr2. */
+		for (int i = k+2; i < n; i++) {
+			arr2[i] = arr1[n-i+k+1];
+		}
 	}
 
 	/* Return N choose K, N >= K. */
@@ -85,14 +111,21 @@ public class Euler051 {
 
 	/* Once smallest lexicographic sequence of L primes is found, prints in format specified. */
 	public static void solution(int N, int K, int L) {
-
+		boolean[][] combs = generateStarArrangements(N,K);
 	}
 	
 	public static void main(String[] args) {
 		sieve();
 		fillTenToThe();
+		boolean[][] test = generateStarArrangements(7,3);
+		for (int i = 0; i < test.length; i++) {
+			for (int j = 0; j < test[i].length; j++) {
+				if (test[i][j]) System.out.print(1 + " ");
+				else System.out.print(0 + " ");
+			}
+			System.out.println();
+		}
 
-		System.out.println(choose(7,3));
 		System.exit(0);
 
 		Scanner s = new Scanner(System.in);

75f3c58a2c48d78d4e01cab4533ec8b0cf026aa7
Troy Feng <troy.feng@yale.edu>
2020-07-09 14:52:46 -0400

    Created class file for problem 52, copied problem statement

---
+++ Euler052.java
@@ -0,0 +1,28 @@
+/* -------- UNSOLVED -------- */
+
+/* It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but 
+ * in a different order.
+ * 
+ * Given N, find all the positive integers, x <= N, such that x, 2x, ..., Kx contain the same digits. 
+ * 
+ * INPUTS: 125875 <= N <= 2000000, 2 <= K <= 6, space-separated N K
+ * OUTPUT: all the K multiples. If greater than one x, print each in a new line.
+ * 
+ * Note 1: It is guaranteed a solution exists.
+ * Note 2: You should not consider solutions with leading 0's. */
+
+import java.util.Scanner;
+
+public class Euler052 {
+	
+	/* Thoughts/approach: */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = Integer.parseInt(s.nextLine());
+		for (int t0 = 0; t0 < t; t0++) {
+			
+		}
+		s.close();
+	}
+}

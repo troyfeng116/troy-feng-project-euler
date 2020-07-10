b8d019fcb11a9831765c0b4b886ed07b2162887c
Troy Feng <troy.feng@yale.edu>
2020-07-10 19:14:32 -0400

    Created class file for problem 56, copied problem description

---
+++ Euler056.java
@@ -0,0 +1,24 @@
+/* -------- UNSOLVED -------- */
+
+/* A googol 10^100 is a massive number: one followed by one-hundred zeros. 100^100 is almost unimaginably 
+ * large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is 
+ * only 1.
+ * 
+ * Considering natural numbers of the form, a^b, where a,b < N, what is the maximum digital sum?
+ *
+ * INPUTS: 5 <= N <= 200
+ * Sample output: N=5 -> 13, as 4^4 = 256, and 2+5+6=13 is the maximal digital sum in that range. */
+
+import java.util.Scanner;
+
+public class Euler056 {
+	
+	/* Thoughts/approach: */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = Integer.parseInt(s.nextLine());
+		
+		s.close();
+	}
+}

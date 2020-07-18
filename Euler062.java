4d990cfef65b4092d4e7ba541c0e4f7722a2be20
Troy Feng <troy.feng@yale.edu>
2020-07-18 17:02:31 -0400

    Created class file for problem 62 and copied problem statement

---
+++ Euler062.java
@@ -0,0 +1,26 @@
+/* -------- UNSOLVED -------- */
+
+/* The cube, 41063625=345^3, can be permuted to produce two other cubes: 56623104=384^3 and 66430125=405^3.
+ * 
+ * In fact, 41063625 is the smallest cube such that exactly three permutations of its digits are also cubes
+ *
+ * You are given N, find the smallest cube for which exactly K permutations of its digits are the cube of 
+ * some number which is < N. If there are multiple sets, print the minimal element of each in sorted order.
+ *
+ * INPUTS: 1000 <= N <= 10^6, 3 <= K <= 49, space separated N K */
+
+import java.util.Scanner;
+
+public class Euler062 {
+	
+	/* Thoughts/approach: */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = Integer.parseInt(s.nextLine());
+		for (int t0 = 0; t0 < t; t0++) {
+			
+		}
+		s.close();
+	}
+}

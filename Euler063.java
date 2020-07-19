c0e4f96c38898470980b326a92d80e3f8f4737b2
Troy Feng <troy.feng@yale.edu>
2020-07-19 15:16:05 -0400

    Created class file for problem 63, copied problem statement

---
+++ Euler063.java
@@ -0,0 +1,25 @@
+/* -------- UNSOLVED -------- */
+
+/* The 5-digit number, 16807 = 7^5, is also a fifth power. Similarly, the 9-digit number, 134217728 = 8^9, 
+ * is a ninth power.
+ *
+ * Given N, print the N-digit positive integers which are also an Nth power. 
+ *
+ * INPUTS: 1 <= N <= 19
+ * OUTPUT each N-digit integer on new line in increasing order. */
+
+import java.util.Scanner;
+
+public class Euler063 {
+	
+	/* Thoughts/approach: */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = Integer.parseInt(s.nextLine());
+		for (int t0 = 0; t0 < t; t0++) {
+			
+		}
+		s.close();
+	}
+}

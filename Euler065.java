7ed38b19c6160ba10d775c5b976b32d24f2d9e24
Troy Feng <troy.feng@yale.edu>
2020-07-22 18:59:07 -0400

    Implemented recursive relation for numerators of e convergent using integers, need to rewrite using BigInteger.

--- Euler065.java
+++ Euler065.java
@@ -37,12 +37,29 @@ import java.util.Scanner;
 
 public class Euler065 {
 	
-	/* Thoughts/approach: */
+	/* Thoughts/approach: There's a nice recursive relation between consecutive numerators and denominators of
+	 * continued fraction convergents. Let u_n denote n'th iteration of continued fraction with [a0,a1,...]
+	 * Note u0 = a0. u1 = a0 + 1/a1 = (a0a1 + 1)/a1. And u2 = a0 + 1/(a1 + 1/a2) = (a2(a1a0+1)+a0)/(a2a1+1).
+	 * Let p_n be the n'th numerator and q_n be the n'th denominator. If we keep expanding, and use induction, 
+	 * we conclude the p_n = a_n * p_{n-1} + p_{n-2} and q_n = a_n * q_{n-1} + q_{n-2}, with p_0 = a0, 
+	 * p_1 = a0*a1+1 and q_0 = 1, q_1 = a_1.
+	 *
+	 * For e, we're given a0 = 2, a1 = 1, a2 = 2, a3 = 1. Then, if k%3=0 or k%3=1, a_k = 1, else a_k = 2*(k/3+1). 
+	 * Finally, we're gonna need BigInteger. */
 	
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int N = Integer.parseInt(s.nextLine());
-		
+		int n2 = 2; /* Numerator 2 terms ago. Initialized to a0 = 2 for e convergent. */
+		int n1 = 3; /* Numerator 1 term ago. Initialized to a1*a0+1 = 3 for e convergent. */
+		int numerator = 3; /* Current numerator term. */
+		for (int k = 2; k < N; k++) {
+			int n = k%3==2 ? 2*(k/3+1) : 1;
+			numerator = n*n1 + n2;
+			n2 = n1;
+			n1 = numerator;
+		}
+		System.out.println(numerator);
 		s.close();
 	}
 }

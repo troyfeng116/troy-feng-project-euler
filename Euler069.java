568e3acd022ca815018b7b595d6844b07b452bff
Troy Feng <troy.feng@yale.edu>
2020-07-26 16:15:05 -0400

    Passed all test cases, will do some precalculations to clean up code and prevent extra space usage

--- Euler069.java
+++ Euler069.java
@@ -24,13 +24,49 @@ import java.util.Scanner;
 
 public class Euler069 {
 	
-	/* Thoughts/approach: */
+	/* Thoughts/approach: Euler's Totient function has a lot of useful properties:
+	 *		https://en.wikipedia.org/wiki/Euler%27s_totient_function
+	 * 
+	 * phi is multiplicative over coprimes. And if p is prime, phi(p^k) = p^k - p^{k-1} = p^k(1 - 1/p). These
+	 * two facts lead us to Euler's product formula: phi(n) = n * PROD_{p|n} (1 - 1/p), where p|n denotes the
+	 * set of distinct primes that divide p. It follows that n/phi(n) = 1/(PROD_{p|n} (1 - 1/p)), meaning we 
+	 * want to minimize the PROD_{p|n} (1 - 1/p), which means we want to maximize the distinct prime factors 
+	 * of n while minimizing n. The smallest n maximizing {p|n} are 2, 2*3, 2*3*5, 2*3*5*7, 2*3*5*7*11,..... T
+	 * hat is, the solution for N is the largest product of consecutive primes < N, starting from 2; there are 
+	 * no smaller numbers with more distinct prime factors. */
+
+	static final long MAX_N = (long) 1E18;
+	static boolean[] composite;
+	/* productPrimes[k] stores the product of the first k (zero-based) primes, i.e. productPrimes[2] = 30. */
+	static long[] productPrimes;
+
+	/* Sieve primes until product exceeds MAX_N. */
+	public static void sieve() {
+		composite = new boolean[200];
+		productPrimes = new long[100];
+		long product = 1;
+		int index = 0;
+		for (int i = 2; i < 500 && product <= MAX_N/i; i++) {
+			if (!composite[i]) {
+				product *= i;
+				productPrimes[index] = product;
+				index++;
+				for (int j = i*i; j < 500; j+=i) {
+					composite[j] = true;
+				}
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
+		sieve();
 		Scanner s = new Scanner(System.in);
 		int t = Integer.parseInt(s.nextLine());
 		for (int t0 = 0; t0 < t; t0++) {
-			
+			long n = Long.parseLong(s.nextLine());
+			int i = 0;
+			while (productPrimes[i+1] != 0 && productPrimes[i+1] < n) i++;
+			System.out.println(productPrimes[i]);
 		}
 		s.close();
 	}

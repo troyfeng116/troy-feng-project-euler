b334b42d37bd80811e9a92fe8733b1ebe39f76d3
Troy Feng <troy.feng@yale.edu>
2020-08-11 14:49:59 -0400

    Created class file for problem 78, copied problem description

---
+++ solutions/Euler078.java
@@ -0,0 +1,33 @@
+/* -------- UNSOLVED -------- */
+
+/* Let p(n) represent the number of different ways in which n coins can be separated into piles. For 
+ * example, five coins can separated into piles in exactly seven different ways, so p(5) = 7.
+ *		OOOOO
+ *		OOOO   O
+ *		OOO   OO
+ *		OOO   O   O
+ *		OO   OO   O
+ *		OO   O   O   O
+ *		O   O   O   O   O
+ *
+ * How many different ways can  coins be separated into piles?
+ *
+ * As answer can be large, print % (10^9+7).
+ *
+ * INPUTS: 1 <= T <= 100, 2 <= N <= 6x10^4 */
+
+import java.util.Scanner;
+
+public class Euler078 {
+	
+	/* Thoughts/approach: */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = Integer.parseInt(s.nextLine());
+		for (int t0 = 0; t0 < t; t0++) {
+			
+		}
+		s.close();
+	}
+}

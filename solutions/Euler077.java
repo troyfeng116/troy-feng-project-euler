b41def2d423f2c740979e0d791663e1cb098e1bf
Troy Feng <troy.feng@yale.edu>
2020-08-11 14:34:56 -0400

    Created class file for problem 77, copied problem statement

---
+++ solutions/Euler077.java
@@ -0,0 +1,29 @@
+/* -------- UNSOLVED -------- */
+
+/* It is possible to write ten as the sum of primes in exactly five different ways:
+ *
+ * 7+3
+ * 5+5
+ * 5+3+2
+ * 3+3+2+2
+ * 2+2+2+2+2
+ *
+ * You are given N, in how many ways can N be written as sum of 1 or more primes?
+ *
+ * INPUTS: 1 <= T <= 100, 2 <= N <= 1000 */
+
+import java.util.Scanner;
+
+public class Euler077 {
+	
+	/* Thoughts/approach: */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = Integer.parseInt(s.nextLine());
+		for (int t0 = 0; t0 < t; t0++) {
+			
+		}
+		s.close();
+	}
+}

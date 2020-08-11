b63c02a5477ad02fa9bb9c7031ca32b64860bc91
Troy Feng <troy.feng@yale.edu>
2020-08-11 11:56:36 -0400

    Created class file for problem 76, copied problem statement

---
+++ solutions/Euler076.java
@@ -0,0 +1,32 @@
+/* -------- UNSOLVED -------- */
+
+/* It is possible to write five as a sum in exactly six different ways:
+ *
+ * 4+1
+ * 3+2
+ * 3+1+1
+ * 2+2+1
+ * 2+1+1+1
+ * 1+1+1+1+1
+ *
+ * How many different ways can  be written as a sum of at least two positive integers?
+ *
+ * As answer can be large, print % (10^9+7).
+ *
+ * INPUTS: 1 <= T <= 100, 2 <= N <= 1000 */
+
+import java.util.Scanner;
+
+public class Euler076 {
+	
+	/* Thoughts/approach: */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = Integer.parseInt(s.nextLine());
+		for (int t0 = 0; t0 < t; t0++) {
+			
+		}
+		s.close();
+	}
+}

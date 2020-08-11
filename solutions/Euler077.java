ff90af7eb6e2e6acee7032592bf2dfb8c716b4cc
Troy Feng <troy.feng@yale.edu>
2020-08-11 14:41:18 -0400

    Implemented sieve and iterative indexing of primes <= 1000

--- solutions/Euler077.java
+++ solutions/Euler077.java
@@ -16,13 +16,47 @@ import java.util.Scanner;
 
 public class Euler077 {
 	
-	/* Thoughts/approach: */
+	/* Thoughts/approach: Exactly the same as the last one, but the set of "coins" is now only the primes.
+	 * Question is: how many ways are there to make change for N using coins={p:<=N}? */
+
+	static final int MAX_N = 1000;
+	/* composite[k] = true iff k is composite. */
+	static boolean[] composite;
+	/* prime[k] holds the k'th prime (zero-based). i.e. prime[0] = 2, prime[1] = 3, etc. */
+	static int[] prime;
+	/* numSums[k] holds the number of ways to write k as the sum of 1+ primes. */
+	static int[] numSums;
+
+	/* Sieve primes up to 1000. Then fill prime[] with indexing of those primes. */
+	public static void sieve() {
+		composite = new boolean[MAX_N+1];
+		int count = 0;
+		for (int i = 2; i*i <= MAX_N; i++) {
+			if (!composite[i]) {
+				count++;
+				for (int j = i*i; j<=MAX_N; j+=i) {
+					composite[j] = true;
+				}
+			}
+		}
+		prime = new int[count];
+		int index = 0;
+		for (int i = 2; i <= MAX_N; i++) {
+			if (!composite[i]) {
+				prime[index] = i;
+				index++;
+			}
+		}
+	}
+
+	/* Compute #ways to write 1000 as sum of prime "coins", memoizing in numSums. */
+	
 	
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int t = Integer.parseInt(s.nextLine());
 		for (int t0 = 0; t0 < t; t0++) {
-			
+			int N = Integer.parseInt(s.nextLine());
 		}
 		s.close();
 	}

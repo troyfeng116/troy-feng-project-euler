fa0d8bd24e8cfa3e55441c1f2d276598fc56256a
Troy Feng <troy.feng@yale.edu>
2020-08-12 17:43:57 -0400

    Created class file for problem 80, copied problem statement

---
+++ solutions/Euler080.java
@@ -0,0 +1,28 @@
+/* -------- UNSOLVED -------- */
+
+/* It is well known that if the square root of a natural number is not an integer, then it is irrational.
+ * The decimal expansion of such square roots is infinite without any repeating pattern at all.
+ *
+ * The square root of two is 1.41421356237309504880..., and the digital sum of the first one hundred 
+ * decimal digits is 475.
+ *
+ * For the first N natural numbers, find the total of the digital sums of the first  digits for all the 
+ * irrational square roots x such that x <= N. 
+ *
+ * INPUTS: either 1 <= N <= 1000 and 1 <= P <= 1000, or 1 <= N <= 100 and 1 <= P <= 10000 */
+
+import java.util.Scanner;
+
+public class Euler080 {
+	
+	/* Thoughts/approach: Python. */
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int t = Integer.parseInt(s.nextLine());
+		for (int t0 = 0; t0 < t; t0++) {
+			
+		}
+		s.close();
+	}
+}
